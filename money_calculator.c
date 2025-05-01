c9cadd438a1909add114381a61dbecef49808959
Alexander Stoilov <151785303+Alex-Stoilov-Dev@users.noreply.github.com>
2025-05-01 21:25:02 +0300

    Update money_calculator.c

--- money_calculator.c
+++ money_calculator.c
@@ -1,261 +1,274 @@
 #include <stdio.h>
 
 int main() {
-	    
-	system("chcp 1251>null");
-	
-	int moneti1, moneti2,moneti5 ,moneti10,moneti20,moneti50,moneti100,moneti200;
-	
-	char simvol;
-	
-	do{
-	
-	printf("Въведете броя на монетите по 1 ст.:");
-	scanf("%d",&moneti1);
-	printf("Въведете броя на монетите по 2 ст.:");
-	scanf("%d",&moneti2);
-	printf("Въведете броя на монетите по 5 ст.:");
-	scanf("%d",&moneti5);
-	printf("Въведете броя на монетите по 10 ст.:");
-	scanf("%d",&moneti10);
-	printf("Въведете броя на монетите по 20 ст.:");
-	scanf("%d",&moneti20);
-	printf("Въведете броя на монетите по 50 ст.:");
-	scanf("%d",&moneti50);
-	printf("Въведете броя на монетите по 1 лв.:");
-	scanf("%d",&moneti100);
-	printf("Въведете броя на монетите по 2 лв.:");
-	scanf("%d",&moneti200);
-	
-	
-	float sumaJulti= (moneti1 * 0.01) + (moneti2 * 0.02) + (moneti5 * 0.05);
-	float sumaBeli= (moneti10 * 0.1) + (moneti20 * 0.2) + (moneti50 * 0.5);
-	float obshtaSuma= sumaJulti + sumaBeli + moneti100 + (moneti200 * 2);
-	
-	int leva = obshtaSuma;
-	int stotinki = (obshtaSuma * 100);
-	stotinki = stotinki%100;
-	
-	printf("\nСумата на жълтите монети: %.2f лв.\n", sumaJulti);
-	printf("Сумата на бели монети: %.2f лв.\n", sumaBeli);
-	printf("Обща сума: %.2f лв.\n", obshtaSuma);
-	printf("Левовете са: %d лв.\n", leva);
-	printf("Стотинките са: %d ст.\n", stotinki);
 
-	int i;
+    // initializing the Bulgarian character set
+    system("chcp 1251>null");
 
-	for (i = 0; i<2 ; i++){
-		int flag = 0;
+    // initializing variables
+    int moneti1, moneti2, moneti5, moneti10, moneti20, moneti50, moneti100, moneti200;
+    
+    char simvol;
+
+    // Make the program reusable, only quit with a specific key press at the end, rather than it being a single use only.
+    do {
 	
-		if( leva> 100){
-			flag = 1;
+	        // user input for the number of coins the user has of each unique kind
+	        printf("Vavedete broi na moneti po 1 stotinka: ");
+	        scanf("%d", &moneti1);
+	        printf("Vavedete broi na moneti po 2 stotinki: ");
+	        scanf("%d", &moneti2);
+	        printf("Vavedete broi na moneti po 5 stotinki: ");
+	        scanf("%d", &moneti5);
+	        printf("Vavedete broi na moneti po 10 stotinki: ");
+	        scanf("%d", &moneti10);
+	        printf("Vavedete broi na moneti po 20 stotinki: ");
+	        scanf("%d", &moneti20);
+	        printf("Vavedete broi na moneti po 50 stotinki: ");
+	        scanf("%d", &moneti50);
+	        printf("Vavedete broi na moneti po 1 lev: ");
+	        scanf("%d", &moneti100);
+	        printf("Vavedete broi na moneti po 2 leva: ");
+	        scanf("%d", &moneti200);
+	        
+	        // Converting the number of coins to the value of said coins. 
+	        // Categorize into white coins (10-50 cents) and yellow coins (1-5 cents) as well as a total sum
+	        float sumaJulti = (moneti1 * 0.01) + (moneti2 * 0.02) + (moneti5 * 0.05);
+	        float sumaBeli = (moneti10 * 0.1) + (moneti20 * 0.2) + (moneti50 * 0.5);
+	        float obshtaSuma = sumaJulti + sumaBeli + moneti100 + (moneti200 * 2);
+	
+	        // Converting the total sum into an int in order to get just the whole part.
+	        // To get the cents we multiply the total sum by 100 and trim the first number off.
+	        int leva = obshtaSuma;
+	        int stotinki = (obshtaSuma * 100);
+	        stotinki = stotinki % 100;
+	
+	        // print the sums of all of the sets of coins as well as the paper money
+	        printf("\nSuma ot julti moneti: %.2f leva.\n", sumaJulti);
+	        printf("Suma ot beli moneti: %.2f leva.\n", sumaBeli);
+	        printf("Obshata suma: %.2f leva.\n", obshtaSuma);
+	        printf("Leva: %d leva.\n", leva);
+	        printf("Stotinki: %d stotinki.\n", stotinki);
+	
+	        // initializing iterable variable
+	        int i;
+	
+	        for (i = 0; i < 2; i++) {
+	            int flag = 0;
+	        
+	            if (leva > 100) {
+	                flag = 1;
+	            }
+	            if (leva > 100 && leva < 200) {
+	                leva = leva - 100;
+	                printf("\nsto ");
+	            }
+	            if (leva > 200 && leva < 300) {
+	                leva = leva - 200;
+	                printf("\nleva ");
+	            }
+	            if (leva > 300 && leva < 400) {
+	                leva = leva - 300;
+	                printf("\ntrista ");
+	            }
+	            if (leva > 400 && leva < 500) {
+	                leva = leva - 400;
+	                printf("\nchetirista ");
+	            }
+	            if (leva > 500 && leva < 600) {
+	                leva = leva - 500;
+	                printf("\npetsta ");
+	            }
+	            if (leva > 600 && leva < 700) {
+	                leva = leva - 600;
+	                printf("\nsheststa ");
+	            }
+	            if (leva > 700 && leva < 800) {
+	                leva = leva - 700;
+	                printf("\nsedemsta ");
+	            }
+	            if (leva > 800 && leva < 900) {
+	                leva = leva - 800;
+	                printf("\nosemsta ");
+	            }
+	            if (leva > 900 && leva < 1000) {
+	                leva = leva - 900;
+	                printf("\ndevetsta ");
+	            }
+	            
+	            if ((leva % 10 == 0 || leva < 10) && flag == 1) {
+	                printf("leva ");
+	            }
+	            if (leva > 20 && leva < 30) {
+	                leva = leva - 20;
+	                printf("dvadeset i ");
+	            }
+	            if (leva > 30 && leva < 40) {
+	                leva = leva - 30;
+	                printf("trideset i ");
+	            }
+	            if (leva > 40 && leva < 50) {
+	                leva = leva - 40;
+	                printf("chetirideset i ");
+	            }
+	            if (leva > 50 && leva < 60) {
+	                leva = leva - 50;
+	                printf("petdeset i ");
+	            }
+	            if (leva > 60 && leva < 70) {
+	                leva = leva - 60;
+	                printf("shestdeset i ");
+	            }
+	            if (leva > 70 && leva < 80) {
+	                leva = leva - 70;
+	                printf("sedemdeset i ");
+	            }
+	            if (leva > 80 && leva < 90) {
+	                leva = leva - 80;
+	                printf("osemdeset i ");
+	            }
+	            if (leva > 90 && leva < 100) {
+	                leva = leva - 90;
+	                printf("devetdeset i ");
+	            }
 		}
-		if(leva > 100 && leva < 200){
-			leva = leva - 100;
-			printf("\nсто ");
-		}
-		if(leva > 200 && leva < 300){
-			leva = leva - 200;
-			printf("\nдвеста ");
-		}
-		if(leva > 300 && leva < 400){
-			leva = leva - 300;
-			printf("\nтриста ");
-		}
-		if(leva > 400 && leva < 500){
-			leva = leva - 400;
-			printf("\nчетиристотин ");
-		}
-		if(leva > 500 && leva < 600){
-			leva = leva - 500;
-			printf("\nпетстотин ");
-		}
-		if(leva > 600 && leva < 700){
-			leva = leva - 600;
-			printf("\nшестотин ");
-		}
-		if(leva > 700 && leva < 800){
-			leva = leva - 700;
-			printf("\nседемстотин ");
-		}
-		if(leva > 800 && leva < 900){
-			leva = leva - 800;
-			printf("\nосемстотин ");
-		}
-		if(leva > 900 && leva < 1000){
-			leva = leva - 900;
-			printf("\nдеветстотин ");
-		}
-		
-		if( (leva % 10 == 0 || leva < 10) && flag == 1){
-			printf("и ");
-		}
-		if(leva > 20 && leva < 30){
-			leva = leva - 20;
-			printf("двадесет и ");
-		}
-		if(leva > 30 && leva < 40){
-			leva = leva - 30;
-			printf("тридесет и ");
-		}
-		if(leva > 40 && leva < 50){
-			leva = leva - 40;
-			printf("четиридесет и ");
-		}
-		if(leva > 50 && leva < 60){
-			leva = leva - 50;
-			printf("петдесет и ");
-		}
-		if(leva > 60 && leva < 70){
-			leva = leva - 60;
-			printf("шестдесет и ");
-		}
-		if(leva > 70 && leva < 80){
-			leva = leva - 70;
-			printf("Седемдесет и ");
-		}
-		if(leva > 80 && leva < 90){
-			leva = leva - 80;
-			printf("осемдесет и ");
-		}if(leva > 90 && leva < 100){
-			leva = leva - 90;
-			printf("деветдесет и ");
-		}
-		
-		switch(leva){
-			case 0:
-				printf("нула");
-				break;
-			case 1:
-				printf("един");
-				break;
-			case 2:
-				printf("два");
-				break;
-			case 3:
-				printf("три");
-				break;
-			case 4:
-				printf("четири");
-				break;
-			case 5:
-				printf("пет");
-				break;
-			case 6:
-				printf("шест");
-				break;
-			case 7:
-				printf("седем");
-				break;
-			case 8:
-				printf("осем");
-				break;
-			case 9:
-				printf("девет");
-				break;
-			case 10:
-				printf("десет");
-				break;
-			case 11:
-				printf("едининадесет");
-				break;
-			case 12:
-				printf("дванадесет");
-				break;
-			case 13:
-				printf("тринадесет");
+	            
+	            switch (leva) {
+	                case 0:
+	                    printf("nul");
+	                    break;
+	                case 1:
+	                    printf("edin");
+	                    break;
+	                case 2:
+	                    printf("dva");
+	                    break;
+	                case 3:
+	                    printf("tri");
+	                    break;
+	                case 4:
+	                    printf("chetiri");
+	                    break;
+	                case 5:
+	                    printf("pet");
+	                    break;
+	                case 6:
+	                    printf("shest");
+	                    break;
+	                case 7:
+	                    printf("sedem");
+	                    break;
+	                case 8:
+	                    printf("osem");
+	                    break;
+	                case 9:
+	                    printf("devet");
+	                    break;
+	                case 10:
+	                    printf("deset");
+	                    break;
+	                case 11:
+	                    printf("edinaeset");
+	                    break;
+	                case 12:
+	                    printf("dvanadeset");
+	                    break;
+	                case 13:
+	                    printf("trinadeset)
 				break;
 			case 14:
-				printf("четиринадесет");
+				printf("chetirinadeset");
 				break;
 			case 15:
-				printf("петнадесет");
+				printf("petnadeset");
 				break;
 			case 16:
-				printf("шестнадесет");
+				printf("shestnadeset");
 				break;
 			case 17:
-				printf("седемнадесет");
+				printf("sedemnadeset");
 				break;
 			case 18:
-				printf("осемнадесет");
+				printf("osemnadeset");
 				break;
 			case 19:
-				printf("деветнадесет");
+				printf("devetnadeset");
 				break;
 			case 20:
-				printf("двадесет");
+				printf("dvadeset");
 				break;
 			case 30:
-				printf("тридесет");
+				printf("trideset");
 				break;
 			case 40:
-				printf("четиридесет");
+				printf("chetirideset");
 				break;
 			case 50:
-				printf("петдесет");
+				printf("petdeset");
 				break;
 			case 60:
-				printf("\nшестдесет");
+				printf("\nshestdeset");
 				break;
 			case 70:
-				printf("\nседемдесет");
+				printf("\nsedemdeset");
 				break;
 			case 80:
-				printf("\nосемдесет");
+				printf("\nosemdeset");
 				break;
 			case 90:
-				printf("\nдеветдесет");
+				printf("\ndevetdeset");
 				break;
 			case 100:
-				printf("сто");
+				printf("sto");
 				break;
 			case 200:
-				printf("двеста");
+				printf("dvesta");
 				break;
 			case 300:
-				printf("триста");
+				printf("trista");
 				break;
 			case 400:
-				printf("четиристотин");
+				printf("chetiristotin");
 				break;
 			case 500:
-				printf("петстотин");
+				printf("petstotin");
 				break;
 			case 600:
-				printf("шестотин");
+				printf("shestotin");
 				break;
 			case 700:
-				printf("седемстотин");
+				printf("sedemstotin");
 				break;
 			case 800:
-				printf("осемстотин");
+				printf("osemstotin");
 				break;
 			case 900:
-				printf("деветстотин");
+				printf("devetstotin");
 				break;
 			case 1000:
-				printf("хиляда");
+				printf("hiljada");
 				break;
 			default:
-				printf("Грешка! Моля въведете число от 0-1000! ");
+				printf("Greshka! Molya vavedete chislo ot 0-1000!");
 		}
-		if(i == 0 ){
-			if(leva == 1){
-				printf(" лев и ");
+
+		// Append the correct currency word based on whether it's the first or second loop pass
+		if(i == 0) {
+			if(leva == 1) {
+				printf(" lev i ");
+			} 
+			else {
+				printf(" leva i ");
 			}
-			else{
-				printf(" лева и ");
-			}
 		}
-		else{
-			printf(" стотинки");
+		else {
+			printf(" stotinki");
 		}
+
+		// Prepare for next iteration (from leva to stotinki)
 		leva = stotinki;
-	}
-	printf("\n\nАко искате да излезете от програмата натиснете -> Q или q");
-	scanf("%s", simvol);
-}while(simvol != 'Q' && simvol != 'q');
-	
-	return 0; 
-}
+		
+		scanf("%s", simvol);  // Read user input into 'simvol'	
+} while(simvol != 'Q' && simvol != 'q');  // Continue loop unless user entered 'Q' or 'q'
+		
+		return 0;
