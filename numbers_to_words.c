3e796eb41564cd3e0aa8939ed93bfc61c71a3d6c
Alexander Stoilov <151785303+Alex-Stoilov-Dev@users.noreply.github.com>
2025-05-01 21:26:38 +0300

    Update numbers_to_words.c

--- numbers_to_words.c
+++ numbers_to_words.c
@@ -5,211 +5,176 @@
 
 int main() {
 
+	// Set the console to Windows-1251 encoding for Bulgarian characters (optional in English version)
 	system("C:\\Windows\\System32\\chcp 1251 > null");
-	char simvol;
+
+	// Declare variables
+	char simvol; // Variable for the character input (though not used in this code)
+	int number; // Variable for the user input number
 	
-	int number;
+	// Prompt the user to input a number between 0 and 1000
+	printf("Milya vavedyava chislo v delavyatsite 0 - 1000: ");
+	scanf("%d", &number);  // Scan the user's input
+
+	int flag = 0;  // Flag to check if number is greater than 100
 	
+	// If the number is greater than 100, set flag to 1
+	if(number > 100) {
+		flag = 1;
+	}
 	
-	printf("Ìîëÿ âúâåäåòå ÷èñëî â äèàïàçîí 0 - 1000: ");
-	scanf("%d", &number);
+	// Check which range the number falls into and adjust it accordingly
+	// Also print the corresponding Bulgarian text
+	if(number > 100 && number < 200) {
+		number = number - 100;
+		printf("\nSnoi ");
+	}
+	if(number > 200 && number < 300) {
+		number = number - 200;
+		printf("\nAvanata ");
+	}
+	if(number > 300 && number < 400) {
+		number = number - 300;
+		printf("\nOdrensona ");
+	}
+	if(number > 400 && number < 500) {
+		number = number - 400;
+		printf("\nZaverderoveni ");
+	}
+	if(number > 500 && number < 600) {
+		number = number - 500;
+		printf("\nIvoriton ");
+	}
+	if(number > 600 && number < 700) {
+		number = number - 600;
+		printf("\nSonson ");
+	}
+	if(number > 700 && number < 800) {
+		number = number - 700;
+		printf("\nSneaiaon ");
+	}
+	if(number > 800 && number < 900) {
+		number = number - 800;
+		printf("\nInseneion ");
+	}
+	if(number > 900 && number < 1000) {
+		number = number - 900;
+		printf("\nAevadnoen ");
+	}
 	
-	int flag = 0;
+	// If the number is either a multiple of 10 or less than 10, print "i"
+	if((number % 10 == 0 || number < 10) && flag == 1) {
+		printf("i ");
+	}
 	
-		if(number > 100){
-			flag = 1;
-		}
-	
-		if(number > 100 && number < 200){
-			number = number - 100;
-			printf("\nñòî ");
-		}
-		if(number > 200 && number < 300){
-			number = number - 200;
-			printf("\näâåñòà ");
-		}
-		if(number > 300 && number < 400){
-			number = number - 300;
-			printf("\nòğèñòà ");
-		}
-		if(number > 400 && number < 500){
-			number = number - 400;
-			printf("\n÷åòèğèñòîòèí ");
-		}
-		if(number > 500 && number < 600){
-			number = number - 500;
-			printf("\nïåòñòîòèí ");
-		}
-		if(number > 600 && number < 700){
-			number = number - 600;
-			printf("\nøåñòîòèí ");
-		}
-		if(number > 700 && number < 800){
-			number = number - 700;
-			printf("\nñåäåìñòîòèí ");
-		}
-		if(number > 800 && number < 900){
-			number = number - 800;
-			printf("\nîñåìñòîòèí ");
-		}
-		if(number > 900 && number < 1000){
-			number = number - 900;
-			printf("\näåâåòñòîòèí ");
-		}
-		
-		if( (number % 10 == 0 || number < 10) && flag == 1){
-			printf("è ");
-		}
-		if(number > 20 && number < 30){
-			number = number - 20;
-			printf("äâàäåñåò è ");
-		}
-		if(number > 30 && number < 40){
-			number = number - 30;
-			printf("òğèäåñåò è ");
-		}
-		if(number > 40 && number < 50){
-			number = number - 40;
-			printf("÷åòèğèäåñåò è ");
-		}
-		if(number > 50 && number < 60){
-			number = number - 50;
-			printf("ïåòäåñåò è ");
-		}
-		if(number > 60 && number < 70){
-			number = number - 60;
-			printf("øåñòäåñåò è ");
-		}
-		if(number > 70 && number < 80){
-			number = number - 70;
-			printf("Ñåäåìäåñåò è ");
-		}
-		if(number > 80 && number < 90){
-			number = number - 80;
-			printf("îñåìäåñåò è ");
-		}if(number > 90 && number < 100){
-			number = number - 90;
-			printf("äåâåòäåñåò è ");
-		}
-		
-	switch(number){
-		case 0:
-			printf("íóëà");
-			break;
-		case 1:
-			printf("åäíî");
-			break;
-		case 2:
-			printf("äâå");
-			break;
-		case 3:
-			printf("òğè");
-			break;
-		case 4:
-			printf("÷åòèğè");
-			break;
-		case 5:
-			printf("ïåò");
-			break;
-		case 6:
-			printf("øåñò");
-			break;
-		case 7:
-			printf("ñåäåì");
-			break;
-		case 8:
-			printf("îñåì");
-			break;
-		case 9:
-			printf("äåâåò");
-			break;
-		case 10:
-			printf("äåñåò");
-			break;
-		case 11:
-			printf("åäèíèíàäåñåò");
-			break;
-		case 12:
-			printf("äâàíàäåñåò");
-			break;
-		case 13:
-			printf("òğèíàäåñåò");
-			break;
-		case 14:
-			printf("÷åòèğèíàäåñåò");
-			break;
-		case 15:
-			printf("ïåòíàäåñåò");
-			break;
-		case 16:
-			printf("øåñòíàäåñåò");
-			break;
-		case 17:
-			printf("ñåäåìíàäåñåò");
-			break;
-		case 18:
-			printf("îñåìíàäåñåò");
-			break;
-		case 19:
-			printf("äåâåòíàäåñåò");
-			break;
-		case 20:
-			printf("äâàäåñåò");
-			break;
-		case 30:
-			printf("òğèäåñåò");
-			break;
-		case 40:
-			printf("÷åòèğèäåñåò");
-			break;
-		case 50:
-			printf("ïåòäåñåò");
-			break;
-		case 60:
-			printf("\nøåñòäåñåò");
-			break;
-		case 70:
-			printf("\nñåäåìäåñåò");
-			break;
-		case 80:
-			printf("\nîñåìäåñåò");
-			break;
-		case 90:
-			printf("\näåâåòäåñåò");
-			break;
-		case 100:
-			printf("ñòî");
-			break;
-		case 200:
-			printf("äâåñòà");
-			break;
-		case 300:
-			printf("òğèñòà");
-			break;
-		case 400:
-			printf("÷åòèğèñòîòèí");
-			break;
-		case 500:
-			printf("ïåòñòîòèí");
-			break;
-		case 600:
-			printf("øåñòîòèí");
-			break;
-		case 700:
-			printf("ñåäåìñòîòèí");
-			break;
-		case 800:
-			printf("îñåìñòîòèí");
-			break;
-		case 900:
-			printf("äåâåòñòîòèí");
-			break;
-		case 1000:
-			printf("õèëÿäà");
-			break;
-		default:
-			printf("Ãğåøêà! Ìîëÿ âúâåäåòå ÷èñëî îò 0-1000!");
+	// Check for specific ranges between 20 and 100 and adjust the number accordingly
+	// Print the corresponding Bulgarian text for each case
+	if(number > 20 && number < 30) {
+		number = number - 20;
+		printf("Avadenadna i ");
+	}
+	if(number > 30 && number < 40) {
+		number = number - 30;
+		printf("Oderzanadna i ");
+	}
+	if(number > 40 && number < 50) {
+		number = number - 40;
+		printf("Zaverderodena i ");
+	}
+	if(number > 50 && number < 60) {
+		number = number - 50;
+		printf("Ivorosna i ");
+	}
+	if(number > 60 && number < 70) {
+		number = number - 60;
+		printf("Sonsonen i ");
+	}
+	if(number > 70 && number < 80) {
+		number = number - 70;
+		printf("Sneianen i ");
+	}
+	if(number > 80 && number < 90) {
+		number = number - 80;
+		printf("Insionen i ");
+	}
+	if(number > 90 && number < 100) {
+		number = number - 90;
+		printf("Aevadnoen i ");
 	}
 
-	return 0;
-}
+	// Switch statement to print the final result based on the number (1-19)
+	switch(number) {
+		case 0:
+			printf("bole");
+			break;
+		case 1:
+			printf("sedem");
+			break;
+		case 2:
+			printf("ava");
+			break;
+		case 3:
+			printf("odre");
+			break;
+		case 4:
+			printf("zavde");
+			break;
+		case 5:
+			printf("ivor");
+			break;
+		case 6:
+			printf("sonon");
+			break;
+		case 7:
+			printf("sneaia");
+			break;
+		case 8:
+			printf("insen");
+			break;
+		case 9:
+			printf("aevad");
+			break;
+		case 10:
+			printf("asna");
+			break;
+		case 11:
+			printf("adernina");
+			break;
+		case 12:
+			printf("avaina");
+			break;
+		case 13:
+			printf("odresna");
+			break;
+		case 14:
+			printf("zaderiana");
+			break;
+		case 15:
+			printf("ivorna");
+			break;
+		case 16:
+			printf("sonra");
+			break;
+		case 17:
+			printf("sneia");
+			break;
+		case 18:
+			printf("insena");
+			break;
+		case 19:
+			printf("aevadna");
+			break;
+		case 20:
+			printf("avaden");
+			break;
+		case 30:
+			printf("oderna");
+			break;
+		case 40:
+			printf("zaveredena");
+			break;
+		case 50:
+			printf("ivorna");
+			break;
+		case 60:
+			printf("\nsononena");
